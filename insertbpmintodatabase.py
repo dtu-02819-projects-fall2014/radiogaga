f7aac072ba4f124e1cc689d96eb7468bc6bfa64a
Rasmus Aaskov <rasmus.aaskov@gmail.com>
2014-11-12 00:41:55 +0100

    pep8 check

--- insertbpmintodatabase.py
+++ insertbpmintodatabase.py
@@ -4,32 +4,31 @@ Created on Mon Oct 20 16:21:07 2014
 
 @author: Søren bærbar
 """
-
-
 from gettempo import getbpm
 import MySQLdb
 import time
 
-db = MySQLdb.connect(host="ip",user="user", passwd="pass",db="dbname")
+# Setup a connection to the MySQL server
+db = MySQLdb.connect(host="ip", user="user", passwd="pass", db="dbname")
 cursor = db.cursor()
 
-
+# Get all elements with bpm = -1
 cursor.execute("SELECT * FROM p3 WHERE bpm = -1")
 
 for row in cursor:
-  track = str(row[0])
-  artist = str(row[1])
-  print(row)
-  bpm = getbpm(artist,track)
-  print('BPM lykkedes')
-  sql_command = """UPDATE p3 SET bpm = '%s' WHERE track='%s'
-                     AND artist = '%s'""" % (bpm,track,artist)
-  try:
-      cursor.execute(sql_command)
-      db.commit()
-  except:
-      print 'ERROR: in update BPM'
-      db.rollback()
-  time.sleep(10) # to avoid error 429: too many requests. Hack
-  
+    track = str(row[0])
+    artist = str(row[1])
+    bpm = getbpm(artist, track)
+    print('BPM lykkedes')
+    sql_command = """UPDATE p3 SET bpm = '%s' WHERE track='%s'
+                    AND artist = '%s'""" % (bpm, track, artist)
+    try:
+        cursor.execute(sql_command)
+        db.commit()
+    except:
+        print('ERROR: in update BPM')
+        db.rollback()
+
+    time.sleep(10)  # to avoid error 429: too many requests. Hack
+
 db.close()
