4e55f4ab6496cf4dd9b90ddd379573bda7b22c8e
Rasmus Aaskov <rasmus.aaskov@gmail.com>
2014-11-19 22:04:28 +0100

    minor changes

--- datamining.py
+++ datamining.py
@@ -5,11 +5,14 @@ import urllib2 as ul
 
 class JsonResponse:
     """Search and return value in json file based on keywords.
+    
     Args:
         json_url: the json address.\n
         keys: a list or tuble of keywords til go through
     Returns:
-        the value returned by the crawling process.
+        answear: the string returned by the crawling process. If the crawling 
+        algorithm did not found the string satisfying all of the keys, it 
+        will return the array containing what have been found so far. 
     """
     def __init__(self, json_url, keywords):
         self.address = json_url
@@ -24,23 +27,14 @@ class JsonResponse:
     def open_connection(self, url):
         request = ul.Request(url, headers={'User-agent': 'Mozilla/5.0'})
         self.data = ul.urlopen(request).read()
-        self.answear = js.loads(self.data)
+        self.answer = js.loads(self.data)
 
     def crawl_page(self, level):
         if level != self.nkeys:
             try:
                 element = self.keywords[level]
-                self.answear = self.answear[element]
+                self.answer = self.answer[element]
                 self.level = self.level + 1
                 self.crawl_page(self.level)
             except:
-                print """No value found at keyword:
-                      %s""" % (self.keywords[level])
-
-# The following provide an example of calling the class on itunes music store
-# searching for Pink Floyd's The Endless River.
-url = """https://itunes.apple.com/search?term=
-         Pink+Floyd+The+Endless+River&limit=1"""
-keywords = ['results', 0, 'artworkUrl100']
-info = JsonResponse(url, keywords)
-print(info.answear)
+                self.answer = ''
